39e4b5c0e0581e9501bd3bfd6a5b253e907a9532
Stas <seeyou85@gmail.com>
2018-05-19 12:07:15 +0300

    Adding editor activity

--- app/src/main/java/com/batura/stas/notesaplication/MainActivity.java
+++ app/src/main/java/com/batura/stas/notesaplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.batura.stas.notesaplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,8 +23,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Add a note", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this,EditorActivity.class) ;
+                startActivity(intent);
             }
         });
     }
